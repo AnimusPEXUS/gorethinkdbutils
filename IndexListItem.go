e2b5a5a942a7e372b8edebcc7c086cadee5cf432
Animus PEXUS <animuspexus@protonmail.com>
2020-11-25 19:48:59 +0300

    init

---
+++ IndexListItem.go
@@ -0,0 +1,11 @@
+package rethinkdbutils
+
+type IndexListItem struct {
+	Index    *string  `gorethink:"index,omitempty"`
+	Ready    *bool    `gorethink:"ready,omitempty"`
+	Progress *float64 `gorethink:"progress,omitempty"`
+	Function []byte   `gorethink:"function,omitempty"`
+	Multi    *bool    `gorethink:"multi,omitempty"`
+	Geo      *bool    `gorethink:"geo,omitempty"`
+	Outdated *bool    `gorethink:"outdated,omitempty"`
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AnimusPEXUS/gorethinkdbutils/rethinkdbutils-go.v6
+
+go 1.14
